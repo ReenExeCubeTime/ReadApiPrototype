e278675e7120b4dff3a3a268a78aaa650f806076
ReenExe <ipourri@gmail.com>
2016-09-28 23:10:53 +0300

    use `doctrine:schema:update`

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,6 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Nelmio\ApiDocBundle\NelmioApiDocBundle(),
-            new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
             new FS\AppBundle\FSAppBundle(),
         ];
 
