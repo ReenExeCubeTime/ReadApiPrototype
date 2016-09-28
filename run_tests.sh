92a439e751b77e9fd06848e72f5ad8e319a92872
ReenExe <ipourri@gmail.com>
2016-09-28 23:49:52 +0300

    default story fixture (#2)

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 php bin/console doctrine:schema:update --force
+php bin/console doctrine:fixtures:load --no-interaction
 php vendor/bin/phpunit
---
+++ src/FS/AppBundle/DataFixtures/ORM/Story.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace FS\AppBundle\DataFixtures\ORM;
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class Story implements FixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+
+    }
+}
