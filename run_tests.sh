90ccede3915f3c2ec8bf8ebad2ea5090a8c22c4c
ReenExe <ipourri@gmail.com>
2016-10-02 11:31:57 +0300

    use common command

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+php bin/console doctrine:database:create --if-not-exists
 php bin/console doctrine:schema:update --force
 php bin/console doctrine:fixtures:load --no-interaction --purge-with-truncate
 php vendor/bin/phpunit
