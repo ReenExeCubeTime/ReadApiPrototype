7473d9e091245f83acc1959c1d3a636a55ce7f9c
ReenExe <ipourri@gmail.com>
2016-10-03 23:23:36 +0300

    generate `Entity`

---
+++ src/FS/AppBundle/Entity/UserStoryFavorite.php
@@ -0,0 +1,171 @@
+<?php
+
+namespace FS\AppBundle\Entity;
+
+/**
+ * UserStoryFavorite
+ */
+class UserStoryFavorite
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var \DateTime
+     */
+    private $created;
+
+    /**
+     * @var \DateTime
+     */
+    private $updated;
+
+    /**
+     * @var \FS\AppBundle\Entity\User
+     */
+    private $user;
+
+    /**
+     * @var \FS\AppBundle\Entity\Story
+     */
+    private $story;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return UserStoryFavorite
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set created
+     *
+     * @param \DateTime $created
+     *
+     * @return UserStoryFavorite
+     */
+    public function setCreated($created)
+    {
+        $this->created = $created;
+
+        return $this;
+    }
+
+    /**
+     * Get created
+     *
+     * @return \DateTime
+     */
+    public function getCreated()
+    {
+        return $this->created;
+    }
+
+    /**
+     * Set updated
+     *
+     * @param \DateTime $updated
+     *
+     * @return UserStoryFavorite
+     */
+    public function setUpdated($updated)
+    {
+        $this->updated = $updated;
+
+        return $this;
+    }
+
+    /**
+     * Get updated
+     *
+     * @return \DateTime
+     */
+    public function getUpdated()
+    {
+        return $this->updated;
+    }
+
+    /**
+     * Set user
+     *
+     * @param \FS\AppBundle\Entity\User $user
+     *
+     * @return UserStoryFavorite
+     */
+    public function setUser(\FS\AppBundle\Entity\User $user = null)
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * Get user
+     *
+     * @return \FS\AppBundle\Entity\User
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * Set story
+     *
+     * @param \FS\AppBundle\Entity\Story $story
+     *
+     * @return UserStoryFavorite
+     */
+    public function setStory(\FS\AppBundle\Entity\Story $story = null)
+    {
+        $this->story = $story;
+
+        return $this;
+    }
+
+    /**
+     * Get story
+     *
+     * @return \FS\AppBundle\Entity\Story
+     */
+    public function getStory()
+    {
+        return $this->story;
+    }
+}
+
