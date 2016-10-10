b6aba7d8bb4baf7b64650e77b39f1e0faa683f57
ReenExe <ipourri@gmail.com>
2016-10-10 22:36:45 +0300

    refactoring: to add more

--- tests/FS/AppBundle/Controller/ApiStoryControllerTest.php
+++ tests/FS/AppBundle/Controller/ApiStoryControllerTest.php
@@ -4,93 +4,110 @@ namespace Tests\FS\AppBundle\Controller;
 
 class ApiStoryControllerTest extends AbstractApiControllerTest
 {
-    public function testList()
+    /**
+     * @dataProvider dataProvider
+     * @param array $parameters
+     * @param array $list
+     * @param array $paging
+     */
+    public function testList(array $parameters, array $list, array $paging)
     {
         $client = static::createClient();
 
-        $client->request('GET', '/api/stories.json');
+        $client->request('GET', '/api/stories.json', $parameters);
 
         $this->expectSuccessStatus($client);
         $this->expectSuccessList(
-            [
-                [
-                    'id' => 1,
-                    'text' => 'Good morning Story',
-                    'category' => [
-                        'name' => 'History',
-                    ],
-                    'language' => [
-                        'code' => 'uk',
-                    ],
-                    'favorite' => [
-                        'total' => 3,
-                        'in' => false,
-                    ],
-                ],
-                [
-                    'id' => 2,
-                    'text' => 'Memory Story',
-                    'category' => [
-                        'name' => 'History',
-                    ],
-                    'language' => [
-                        'code' => 'uk',
-                    ],
-                    'favorite' => [
-                        'total' => 1,
-                        'in' => false,
-                    ],
-                ],
-                [
-                    'id' => 3,
-                    'text' => 'Try Story',
-                    'category' => [
-                        'name' => 'History',
-                    ],
-                    'language' => [
-                        'code' => 'uk',
-                    ],
-                    'favorite' => [
-                        'total' => 0,
-                        'in' => false,
-                    ],
-                ],
-                [
-                    'id' => 4,
-                    'text' => 'Funny',
-                    'category' => [
-                        'name' => 'History',
-                    ],
-                    'language' => [
-                        'code' => 'uk',
-                    ],
-                    'favorite' => [
-                        'total' => 0,
-                        'in' => false,
-                    ],
-                ],
-                [
-                    'id' => 5,
-                    'text' => 'Little',
-                    'category' => [
-                        'name' => 'History',
-                    ],
-                    'language' => [
-                        'code' => 'uk',
-                    ],
-                    'favorite' => [
-                        'total' => 0,
-                        'in' => false,
-                    ],
-                ],
-            ],
-            [
-                'page' => 1,
-                'pages' => 1,
-                'limit' => 10,
-                'total' => 5,
-            ],
+            $list,
+            $paging,
             $client
         );
+    }
+
+    public function dataProvider()
+    {
+        return [
+            [
+                [],
+                [
+                    [
+                        'id' => 1,
+                        'text' => 'Good morning Story',
+                        'category' => [
+                            'name' => 'History',
+                        ],
+                        'language' => [
+                            'code' => 'uk',
+                        ],
+                        'favorite' => [
+                            'total' => 3,
+                            'in' => false,
+                        ],
+                    ],
+                    [
+                        'id' => 2,
+                        'text' => 'Memory Story',
+                        'category' => [
+                            'name' => 'History',
+                        ],
+                        'language' => [
+                            'code' => 'uk',
+                        ],
+                        'favorite' => [
+                            'total' => 1,
+                            'in' => false,
+                        ],
+                    ],
+                    [
+                        'id' => 3,
+                        'text' => 'Try Story',
+                        'category' => [
+                            'name' => 'History',
+                        ],
+                        'language' => [
+                            'code' => 'uk',
+                        ],
+                        'favorite' => [
+                            'total' => 0,
+                            'in' => false,
+                        ],
+                    ],
+                    [
+                        'id' => 4,
+                        'text' => 'Funny',
+                        'category' => [
+                            'name' => 'History',
+                        ],
+                        'language' => [
+                            'code' => 'uk',
+                        ],
+                        'favorite' => [
+                            'total' => 0,
+                            'in' => false,
+                        ],
+                    ],
+                    [
+                        'id' => 5,
+                        'text' => 'Little',
+                        'category' => [
+                            'name' => 'History',
+                        ],
+                        'language' => [
+                            'code' => 'uk',
+                        ],
+                        'favorite' => [
+                            'total' => 0,
+                            'in' => false,
+                        ],
+                    ],
+                ],
+                [
+                    'page' => 1,
+                    'pages' => 1,
+                    'limit' => 10,
+                    'total' => 5,
+                ],
+            ]
+        ];
     }
 }
