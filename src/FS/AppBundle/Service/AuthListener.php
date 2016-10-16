9474bca050c2feeadf6adf33ff750c8725e84d19
ReenExe <ipourri@gmail.com>
2016-10-16 19:03:52 +0300

    default `Auth Listener` (#8)

---
+++ src/FS/AppBundle/Service/AuthListener.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace FS\AppBundle\Service;
+
+use Symfony\Component\HttpKernel\Event\KernelEvent;
+
+class AuthListener
+{
+    public function onKernelRequest(KernelEvent $event)
+    {
+        
+    }
+}
